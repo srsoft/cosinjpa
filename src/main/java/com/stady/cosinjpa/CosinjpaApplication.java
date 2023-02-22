3a73c605f1b079da3cef03a282c522dc6220238b
daniel <stevegates1013@gmail.com>
2023-02-22 15:11:32 +0900

    first

---
+++ src/main/java/com/stady/cosinjpa/CosinjpaApplication.java
@@ -0,0 +1,13 @@
+package com.stady.cosinjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosinjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosinjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stady/cosinjpa/CosinjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stady.cosinjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosinjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
