1706ce4b1cc8a13d51d55901445064d7020deb43
daniel <stevegates1013@gmail.com>
2023-02-23 17:52:55 +0900

    02/23 3

--- src/main/java/com/stady/cosinjpa/controller/BoardController.java
+++ src/main/java/com/stady/cosinjpa/controller/BoardController.java
@@ -5,11 +5,10 @@ import com.stady.cosinjpa.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -27,13 +26,21 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model) {
-        model.addAttribute("board", new Board());
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute(board);
+        }
         return "board/form";
     }
 
     @PostMapping("/form")
-    public String form(@ModelAttribute Board board) {
+    public String form(@Valid Board board, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
         boardRepository.save(board);
         return "redirect:/board/list";
     }
--- src/main/java/com/stady/cosinjpa/model/Board.java
+++ src/main/java/com/stady/cosinjpa/model/Board.java
@@ -6,6 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Data
@@ -13,6 +17,12 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty
+    @Size(min=2, max=30)
     private String title;
+
+    @NotEmpty
+    @Size(min=2, max=30)
     private String content;
 }
