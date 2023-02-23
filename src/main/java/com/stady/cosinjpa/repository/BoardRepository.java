bf49b1a7907b74a7647100c90f5f895f6f15a27c
daniel <stevegates1013@gmail.com>
2023-02-23 17:03:48 +0900

    02/23 1

---
+++ src/main/java/com/stady/cosinjpa/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.stady.cosinjpa.repository;
+
+import com.stady.cosinjpa.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
